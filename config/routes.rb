4af6b14bc4b2e7ec09a7255519fe8eea35f75685
Hal Evans <hal.evans@newtoneurope.com>
2023-03-06 10:49:25 +0000

    Add a Home Route and a Route to Mount The React App

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'welcome/home'
+  get '/app', to: 'welcome#app', as: 'app'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
 end
