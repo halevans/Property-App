6ad3c356854d372bf5bf10f8f29a1a6c44067f73
Hal Evans <hal.evans@newtoneurope.com>
2023-03-06 13:55:04 +0000

    Restarting repo

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/home'
-  get '/app', to: 'welcome#app', as: 'app'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-end
