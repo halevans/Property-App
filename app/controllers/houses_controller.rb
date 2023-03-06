a6d80a8865181e9074303bb53de9ade7110d1063
Hal Evans <hal.evans@newtoneurope.com>
2023-03-06 16:51:49 +0000

    added create method to house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -11,6 +11,16 @@ class HousesController < ApplicationController
     render json: @house
   end
 
+  def create
+    @house = House.new(params.require(:house).permit(:asking_price, :no_rooms, :img_url, :user_id))
+
+    if @house.save
+      render json: @house, status: :created, location: @house
+    else
+      render json: @house.errors, status: :unprocessable_entity
+    end
+  end
+
   private 
   
   def set_house
--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,19 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :update, :destroy]
+
   def index
     @offers = Offer.all
 
     render json: @offers
+  end
+
+  def show
+    render json: @offer
+  end
+
+  private 
+  
+  def set_offer
+    @offer = Offer.find(params[:id])
   end
 end
