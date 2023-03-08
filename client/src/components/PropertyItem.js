71c98d0a695674a37a0530f57dd724ee0eb0e37d
Tom Fairclough <tom_fairclough@btinternet.com>
2023-03-08 13:51:52 +0000

    TF: style

--- client/src/components/PropertyItem.js
+++ client/src/components/PropertyItem.js
@@ -1,13 +1,22 @@
+import { Container, Navbar, Nav, Button, Card } from 'react-bootstrap'
 import React from 'react'
+
 
 function PropertyItem(props) {
   return (
     <>
     <h1>PropertyItem</h1>
-    <p>{props.propertyDetails.asking_price}</p>
-    <p>{props.propertyDetails.no_rooms}</p>
-    <p>{props.propertyDetails.img_url}</p>
+    <Container>
+      <Card>
+        <Card.Body>
+          <Card.Img variant='left' style={{height: '5rem', objectFit: 'cover', display: 'inline'}} src={props.propertyDetails.img_url}/>
+          <Card.Title>{props.propertyDetails.asking_price}</Card.Title>
+          <Card.Text>{props.propertyDetails.no_rooms}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
     </>
+
   )
 }
 
