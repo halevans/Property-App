724ed0dd486ef9378721a90adf7baf1712188d02
Tom Fairclough <tom_fairclough@btinternet.com>
2023-03-08 14:05:24 +0000

    TF: Fixed Controller Devise Routes

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,4 +1,21 @@
 class TokensController < Devise::Api::TokensController
+  
+  def sign_in
+    super
+  end
+
+  def revoke
+    super
+  end
+
+  def info
+    super
+  end
+
+  def refresh
+    super
+  end
+
   private
   def sign_up_params
     params.permit(:first_name, :last_name, :phone_number, *resource_class.authentication_keys,
